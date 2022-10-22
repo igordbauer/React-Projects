7b68c746afab3df11a09a6aa3c31bdf29f5c0aba
igordbauer <igordbauer@gmail.com>
2022-10-22 09:50:46 -0300

    add error handling

--- foodapp/src/components/Meals/AvailableMeals.js
+++ foodapp/src/components/Meals/AvailableMeals.js
@@ -8,10 +8,16 @@ import { useEffect, useState } from 'react';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] =useState(true)
+  const [httpError,setHttpError] = useState(null)
 
 useEffect(()=>{
  const fetchMeals = async ()=>{
   const response = await fetch('https://react-custom-hooks-94454-default-rtdb.firebaseio.com/meals.json')
+ 
+ if(response.ok){
+  throw new Error('Something went wrong');
+ }
+
   const responseData = await response.json();
 
   const loadedMeals =[];
@@ -27,7 +33,11 @@ useEffect(()=>{
   setMeals(loadedMeals)
   setIsLoading(false)
 }
-fetchMeals()
+  fetchMeals().catch((error)=>{
+    setIsLoading(false)
+    setHttpError(error.message)
+
+  });
 },[])
 
 if(isLoading){
@@ -37,7 +47,13 @@ if(isLoading){
     </section>
   )
 }
-
+if(httpError){
+  return(
+    <section className={classes.MealsError}>
+      {httpError}
+    </section>
+  )
+}
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
