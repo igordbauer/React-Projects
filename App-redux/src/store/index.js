7a10347574931c383291020a60f62661471356f0
igordbauer <igordbauer@gmail.com>
2022-10-26 18:40:07 -0300

    finish-first-react-redux-demo

--- App-redux/src/store/index.js
+++ App-redux/src/store/index.js
@@ -1,16 +1,17 @@
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action === 'increment') {
+  if (action.type === 'increment') {
     return {
       counter: state.counter + 1
     }
   }
-  if (action === 'decrement') {
+  if (action.type === 'decrement') {
     return {
       counter: state.counter - 1
     }
   }
+  return state
 }
 
 const store = createStore(counterReducer)
