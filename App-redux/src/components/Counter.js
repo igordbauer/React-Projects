be7be75f75fa5130f976ea4b07573b76062df22e
igordbauer <igordbauer@gmail.com>
2022-10-27 09:15:10 -0300

    working-with-multiple-state-redux

--- App-redux/src/components/Counter.js
+++ App-redux/src/components/Counter.js
@@ -4,22 +4,31 @@ const Counter = () => {
 
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' })
   }
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', value: 5 })
+  }
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' })
   }
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- App-redux/src/store/index.js
+++ App-redux/src/store/index.js
@@ -1,14 +1,31 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
-      counter: state.counter + 1
+      ...state,
+      counter: state.counter + 1,
     }
   }
   if (action.type === 'decrement') {
     return {
-      counter: state.counter - 1
+      ...state,
+      counter: state.counter - 1,
+
+    }
+  }
+  if (action.type === 'increase') {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    }
+  }
+  if (action.type === 'toggle') {
+    return {
+      ...state,
+      showCounter: !state.showCounter
     }
   }
   return state
