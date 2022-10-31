7f159ce742ce609f1c25634ddf5a43af5dbc6b23
igordbauer <igordbauer@gmail.com>
2022-10-31 11:09:57 -0300

    hot-fix

--- App-redux/src/components/Header.js
+++ App-redux/src/components/Header.js
@@ -20,15 +20,17 @@ const Header = () => {
       <h1>Redux Auth</h1>
       <nav>
         <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          {isAuthenticated && <li>
-            <button onClick={logoutHandler}>Logout</button>
-          </li>}
+          {isAuthenticated && <>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </>}
         </ul>
       </nav>
     </header>
