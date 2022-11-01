7295a4bf1452268cc62b5112a647a1acdad07b16
igordbauer <igordbauer@gmail.com>
2022-11-01 11:29:55 -0300

    create-removeItemFromCart-method

--- Advanced-Redux/01-starting-project/01-starting-project/src/store/cartSlice.js
+++ Advanced-Redux/01-starting-project/01-starting-project/src/store/cartSlice.js
@@ -23,6 +23,16 @@ const cartSlice = createSlice({
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id)
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+      }
     }
   }
 })
