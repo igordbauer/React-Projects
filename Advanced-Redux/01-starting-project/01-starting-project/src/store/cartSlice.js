5be6b99b44f1d5918cc6babaf82d7c1c8c6c2b90
igordbauer <igordbauer@gmail.com>
2022-11-01 11:21:08 -0300

    configure-ui-and-cart-slices

---
+++ Advanced-Redux/01-starting-project/01-starting-project/src/store/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.paylod
+      const existingItem = state.items.find(item => item.id === newItem.id)
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        })
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    }
+  }
+})
+
+
+export const cartActions = cartSlice.actions
+
+export default cartSlice;
