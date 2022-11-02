fe1f7262a985f783f03240477c5777f84301ed24
igordbauer <igordbauer@gmail.com>
2022-11-01 23:03:00 -0300

    finish-remaining-features

--- Advanced-Redux/01-starting-project/01-starting-project/src/components/Cart/CartButton.js
+++ Advanced-Redux/01-starting-project/01-starting-project/src/components/Cart/CartButton.js
@@ -1,11 +1,13 @@
 import classes from './CartButton.module.css';
 import { useDispatch } from 'react-redux';
 import { uiActions } from '../../store/uiSlice';
+import { useSelector } from 'react-redux';
+
 
 const CartButton = (props) => {
+  const cartQuantity = useSelector(state => state.cart.totalQuantity)
 
   const dispatch = useDispatch();
-
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
   }
@@ -13,7 +15,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- Advanced-Redux/01-starting-project/01-starting-project/src/store/cartSlice.js
+++ Advanced-Redux/01-starting-project/01-starting-project/src/store/cartSlice.js
@@ -9,11 +9,12 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItemToCart(state, action) {
-      const newItem = action.paylod
+      const newItem = action.payload
       const existingItem = state.items.find(item => item.id === newItem.id)
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -27,6 +28,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id)
       } else {
