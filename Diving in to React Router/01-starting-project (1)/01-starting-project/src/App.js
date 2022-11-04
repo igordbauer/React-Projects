346f623154c8096d38a6f3ee2c40b5a7f2b8d430
igordbauer <igordbauer@gmail.com>
2022-11-04 09:32:37 -0300

    understanding-basically-how-it-works

--- Diving in to React Router/01-starting-project (1)/01-starting-project/src/App.js
+++ Diving in to React Router/01-starting-project (1)/01-starting-project/src/App.js
@@ -1,7 +1,16 @@
+import { Route } from 'react-router-dom'
+import Welcome from './pages/Welcome'
+import Product from './pages/Product'
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path='/welcome'>
+        <Welcome />
+      </Route>
+      <Route path='/product'>
+        <Product />
+      </Route>
     </div>
   );
 }
