9fb57cbf0fd54dc91d8f2ba6ddd3df7b5bef59b9
IgorDalpiaz <igor.dalpiaz@networkme.io>
2022-11-09 21:11:35 -0300

    setting-detail-quote

--- Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,15 +1,15 @@
 import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-    { id: 'q2,', author: 'Igor', text: 'Im little bit tired!' }
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Igor', text: 'Im little bit tired!' }
 ]
 
 
 
 const AllQuotes = () => {
-    return (
-        <QuoteList quotes={DUMMY_QUOTES} />
-    )
+  return (
+    <QuoteList quotes={DUMMY_QUOTES} />
+  )
 }
 export default AllQuotes;
--- Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,17 +1,29 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Igor', text: 'Im little bit tired!' }
+]
 
 const QuoteDetail = () => {
 
   const params = useParams()
 
-  return <>
-    <h1>QuoteDetail</h1>
-    <p>{params.quoteId}</p>
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
 
-    <Route path={`/quotes/${params.quoteId}/comments`}>
-      <Comments />
-    </Route>
-  </>
+  if (!quote){
+    return <p>no quote found!</p>
+  }
+
+    return <>
+      <h1>QuoteDetail</h1>
+      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
 }
 export default QuoteDetail;
