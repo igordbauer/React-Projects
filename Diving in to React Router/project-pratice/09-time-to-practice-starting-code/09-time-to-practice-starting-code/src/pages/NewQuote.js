553728d71b3b8c5e967a75a3ea3e65df0db50ee4
igordbauer <igordbauer@gmail.com>
2022-11-15 08:51:41 -0300

    add-quote-api

--- Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/NewQuote.js
+++ Diving in to React Router/project-pratice/09-time-to-practice-starting-code/09-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,16 +1,27 @@
+import { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
 
-  const history = useHistory()
+  const { sendRequest, status } = useHttp(addQuote);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes')
+    }
+  })
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
-    history.push('/quotes')
+    sendRequest(quoteData)
   }
 
   return (
-    <QuoteForm onAddQuote={addQuoteHandler} />
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
   )
 }
 export default NewQuote;
