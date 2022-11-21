f60493975dc9f2de4f3a48cdb09247cf6b1ce49c
igordbauer <igordbauer@gmail.com>
2022-11-21 09:05:25 -0300

    setup-context

--- Authentication-react-project/auth-project/src/components/Layout/MainNavigation.js
+++ Authentication-react-project/auth-project/src/components/Layout/MainNavigation.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
-
+import { useContext } from 'react';
 import classes from './MainNavigation.module.css';
+import AuthContext from '../../store/authContext';
 
 const MainNavigation = () => {
+
+  const { isLoggedIn } = useContext(AuthContext)
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,12 +14,16 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
+          {!isLoggedIn &&
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          }
+          {isLoggedIn &&
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          }
           <li>
             <button>Logout</button>
           </li>
