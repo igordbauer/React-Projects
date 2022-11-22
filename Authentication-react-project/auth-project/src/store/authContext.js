f90c88035b4b494b2007d06d53b0b6fe123186cd
igordbauer <igordbauer@gmail.com>
2022-11-22 09:22:00 -0300

    persistent-auth

--- Authentication-react-project/auth-project/src/store/authContext.js
+++ Authentication-react-project/auth-project/src/store/authContext.js
@@ -8,16 +8,19 @@ const AuthContext = createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null)
+  const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken)
 
 
   const userIsLoggedIn = Boolean(token)
 
   const loginHandler = (token) => {
     setToken(token)
+    localStorage.setItem('token', token)
   }
   const logoutHandler = () => {
     setToken(null)
+    localStorage.removeItem('token')
   }
 
   const contextValue = {
