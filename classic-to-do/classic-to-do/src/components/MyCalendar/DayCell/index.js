20b3849e58775f3c55c51ee12ca3eff68ac8d0f9
igordbauer <igordbauer@gmail.com>
2022-12-02 12:55:46 -0300

    nulldays-style

--- classic-to-do/classic-to-do/src/components/MyCalendar/DayCell/index.js
+++ classic-to-do/classic-to-do/src/components/MyCalendar/DayCell/index.js
@@ -1,6 +1,7 @@
 import classes from './DayCell.module.css'
 
 const DayCell = ({ day }) => {
-  return <button className={classes.daycell}>{day === -1 ? null : day}</button>
+  const classe = day!==-1 ? classes.daycell : classes.daycellnull
+  return <button className={classe}>{day === -1 ? null : day}</button>
 }
 export default DayCell;
