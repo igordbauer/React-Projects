9ba1e884592556ea5783286b5c63ec355f3411a8
igordbauer <igordbauer@gmail.com>
2022-12-27 23:05:51 -0300

    configure-prettier

--- INTL javascript/index.js
+++ INTL javascript/index.js
@@ -1,56 +1,53 @@
-
 const DIVISIONS = [
-    { amount: 60, name: 'seconds' },
-    { amount: 60, name: 'minutes' },
-    { amount: 24, name: 'hours' },
-    { amount: 7, name: 'days' },
-    { amount: 4.34524, name: 'weeks' },
-    { amount: 12, name: 'months' },
-    { amount: Number.POSITIVE_INFINITY, name: 'years' },
-]
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+];
 
 //----------------------------------------------------------------
 
-const english = new Intl.RelativeTimeFormat('en-us')
-const brazilian = new Intl.RelativeTimeFormat('pt-br')
+const english = new Intl.RelativeTimeFormat("en-us");
+const brazilian = new Intl.RelativeTimeFormat("pt-br");
 
 //style
 
-const long = new Intl.RelativeTimeFormat('en-us', { style: 'long' })
-const short = new Intl.RelativeTimeFormat('en-us', { style: 'short' })
-const narrow = new Intl.RelativeTimeFormat('en-us', { style: 'narrow' })
+const long = new Intl.RelativeTimeFormat("en-us", { style: "long" });
+const short = new Intl.RelativeTimeFormat("en-us", { style: "short" });
+const narrow = new Intl.RelativeTimeFormat("en-us", { style: "narrow" });
 
 //This determines if the formatted value should always be numeric
 
-const always = new Intl.RelativeTimeFormat('en-us', { numeric: 'always' })
-const auto = new Intl.RelativeTimeFormat('pt-br', { numeric: 'auto' })
+const always = new Intl.RelativeTimeFormat("en-us", { numeric: "always" });
+const auto = new Intl.RelativeTimeFormat("pt-br", { numeric: "auto" });
 
 // practice
 
 const formatter = new Intl.RelativeTimeFormat(undefined, {
-    numeric: 'auto'
-})
+  numeric: "auto",
+});
 const formatTime = (date) => {
+  let duration = (date - new Date()) / 1000;
 
-    let duration = (date - new Date()) / 1000
-
-    for (let i = 0; i <= DIVISIONS.length; i++) {
-        const division = DIVISIONS[i]
-        if (Math.abs(duration) < division.amount) {
-            return formatter.format(Math.round(duration), division.name)
-        }
-        duration /= division.amount
+  for (let i = 0; i <= DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name);
     }
-}
-const currentDate = new Date()
+    duration /= division.amount;
+  }
+};
+const currentDate = new Date();
 
-console.log(formatTime(new Date().setMonth(currentDate.getMonth() - 2)))
+console.log(formatTime(new Date().setMonth(currentDate.getMonth() - 2)));
 // 2 months ago
-console.log(formatTime(new Date().setDate(currentDate.getDate() - 1)))
+console.log(formatTime(new Date().setDate(currentDate.getDate() - 1)));
 // yesterday
-console.log(formatTime(new Date().setDate(currentDate.getDate() - 9)))
+console.log(formatTime(new Date().setDate(currentDate.getDate() - 9)));
 // last week
-
 
 // console.log(always.format(0, 'hours'))
 // console.log(auto.format(0, 'hours'))
