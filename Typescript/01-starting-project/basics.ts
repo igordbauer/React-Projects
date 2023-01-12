88d968e8ad9954a415a1c9fcfff5b42dd7dc92dd
igordbauer <igordbauer@gmail.com>
2023-01-12 11:19:12 -0300

    starting-with-typescript

---
+++ Typescript/01-starting-project/basics.ts
@@ -0,0 +1,28 @@
+// Primitive: number, string and boolean
+// arrays and objects
+// function types and parameters
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let age: number = 23;
+
+let username: string = "asdasd";
+
+let array: string[] = ["asdasd", "asdasd"];
+
+let igor: Person = {
+  name: "igor",
+  age: 23,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+let course: string | number = "React";
+
+course = 89334;
