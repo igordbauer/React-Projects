033d650b5f20ee6332499776fe9b03b1787e1a1b
igordbauer <igordbauer@gmail.com>
2023-01-21 21:37:40 -0300

    adding-center-label2

--- classic-to-do/classic-to-do/src/components/Layout/index.js
+++ classic-to-do/classic-to-do/src/components/Layout/index.js
@@ -1,9 +1,12 @@
 import classes from "./Layout.module.css";
-
+import { useSelector } from "react-redux";
 const Layout = ({ children }) => {
+  const choosenDay = useSelector((state) => state.chosenDayReducer);
   return (
     <>
-      <div className={classes.label}>igor</div>
+      <div className={classes.label}>
+        {choosenDay.month + " " + choosenDay.day + " " + choosenDay.weekDay}
+      </div>
       <main className={classes.main}>{children}</main>
     </>
   );
